13887baa362ccb2fc5903ae7efc28a89cf06a18b
Bruno Morais <bruno.morais777@gmail.com>
2021-04-13 19:29:50 +0100

    removed unecessary method override from PKEdgeRDD

--- src/main/scala/org/apache/spark/graphx/pkgraph/graph/PKEdgeRDD.scala
+++ src/main/scala/org/apache/spark/graphx/pkgraph/graph/PKEdgeRDD.scala
@@ -1,7 +1,7 @@
 package org.apache.spark.graphx.pkgraph.graph
 
 import org.apache.spark.graphx.impl.EdgePartition
-import org.apache.spark.graphx.{EdgeRDD, PartitionID, VertexId}
+import org.apache.spark.graphx.{EdgeRDD, PartitionID}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.OneToOneDependency
 import org.apache.spark.storage.StorageLevel
@@ -26,19 +26,4 @@ abstract class PKEdgeRDD[E: ClassTag](rdd: RDD[_]) extends EdgeRDD[E](rdd.contex
    * @return [[NotImplementedError]]
    */
   override def withTargetStorageLevel(targetStorageLevel: StorageLevel): EdgeRDD[E] = throw new NotImplementedError
-
-  /**
-    * Inner joins this EdgeRDD with another [[PKEdgeRDD]], assuming both are partitioned using the same
-    * [[PartitionStrategy]].
-    *
-    * @param other the [[PKEdgeRDD]] to join with
-    * @param f the join function applied to corresponding values of `this` and `other`
-    * @tparam E2 Type of edge attribute of the RDD to join with
-    * @tparam E3 Resulting type of edge attribute
-    * @return a new EdgeRDD containing only edges that appear in both `this` and `other`,
-    *         with values supplied by `f`
-    */
-  override def innerJoin[E2: ClassTag, E3: ClassTag](other: EdgeRDD[E2])(
-      f: (VertexId, VertexId, E, E2) => E3
-  ): PKEdgeRDD[E3]
 }
